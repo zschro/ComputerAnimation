491b0a26b75ea014a5143d87afb1677c6a608a8a
Zachary Schroeder <z.schro@gmail.com>
2017-03-26 01:30:16 -0400

    reparameterized arc length

--- Lab5/Assets/CatmullRomCurveInterpolation.cs
+++ Lab5/Assets/CatmullRomCurveInterpolation.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class CatmullRomCurveInterpolation : MonoBehaviour {
 	
 	const int NumberOfPoints = 8;
 	Vector3[] controlPoints;
-
     Vector3[] subPoints;
+	List<Vector3> travelPoints;
+	int nextTravelPoint = 0;
 
     double[] arclengths = new double[NumberOfPoints];
     float totalLength;
@@ -37,7 +39,7 @@ public class CatmullRomCurveInterpolation : MonoBehaviour {
 		int p_1 = (segmentNumber + 1) % (NumberOfPoints);
 
 
-		Debug.Log ($"p0: {p0}, p_1: {p_1}, p_-2: {p_minus_2}");
+		//Debug.Log ($"p0: {p0}, p_1: {p_1}, p_-2: {p_minus_2}");
 
 		Vector4 uVector = new Vector4 (u * u * u, u * u, u, 1);
 		var xVector = new Vector4(controlPoints[p_minus_2].x,controlPoints[p_minus_1].x,
@@ -83,62 +85,78 @@ public class CatmullRomCurveInterpolation : MonoBehaviour {
 			controlPoints[i] = new Vector3(Random.Range(MinX,MaxX),Random.Range(MinY,MaxY),Random.Range(MinZ,MaxZ));
         }
 
-        float clength = 0;
-        subPoints = new Vector3[NumberOfPoints * 10];
-        Vector3 dpoint;
-        for (int i = 0; i < NumberOfPoints; i++)
-        {
-            for (int j = 0; j < 10; j++)
-            {
-                //next point
-                Vector3 furtherPoint = controlPoints[i] * (.1f * j);
-                subPoints[(i * 10) + j] = furtherPoint;
-                //calculate distance between new point and old point
-                if(i + j == 0)
-                {
-                    dpoint = subPoints[0] - furtherPoint;
-                }
-                else
-                {
-                    dpoint = subPoints[(i * 10) + j - 1] - furtherPoint;
-                }
-                
-                //calculate arclength, add to running total and running total arraylist
-                float preRoot = Mathf.Pow(dpoint.x, 2) + Mathf.Pow(dpoint.y, 2) + Mathf.Pow(dpoint.z, 2);
-                clength += Mathf.Sqrt(preRoot);
-
-                arclengths[i] = clength;
-                //Debug.Log(arclengths[i]);
-
-            }
-        }
-
-        totalLength += clength;
-        Debug.Log(totalLength);
-
         GenerateControlPointGeometry();
+
 		catmulRom.SetRow (0, new Vector4 (-0.5f, 1.5f, -1.5f, 0.5f));
 		catmulRom.SetRow (1, new Vector4 (1.0f, -2.5f, 2.0f, -0.5f));
 		catmulRom.SetRow (2, new Vector4 (-0.5f, 0f, 0.5f, 0f));
 		catmulRom.SetRow (3, new Vector4 (0f, 1.0f, 0f, 0f));
+		ReparameterizeArcLength ();
 	}
-	
+
+	private void ReparameterizeArcLength(){
+		float clength = 0;
+		subPoints = new Vector3[NumberOfPoints * 10000];
+		travelPoints = new List<Vector3>();
+		travelPoints.Add(ComputePointOnCatmullRomCurve(0,0));
+		float subsectionLength = 0;
+
+		Vector3 dpoint;
+		for (int i = 0; i < NumberOfPoints; i++)
+		{
+			for (int j = 0; j < 10000; j++)
+			{
+				//next point
+				//Vector3 furtherPoint = controlPoints[i] * (.1f * j);
+				Vector3 furtherPoint = ComputePointOnCatmullRomCurve(j * 0.0001f,i);
+				subPoints[(i * 10000) + j] = furtherPoint;
+				//calculate distance between new point and old point
+				if(i + j == 0)
+				{
+					dpoint = new Vector3 ();
+					//dpoint = subPoints[0] - furtherPoint;
+				}
+				else
+				{
+					dpoint = subPoints[(i * 10000) + j - 1] - furtherPoint;
+				}
+				subsectionLength += dpoint.magnitude;
+				if (subsectionLength > .03f) {
+					travelPoints.Add (furtherPoint);
+					subsectionLength = 0.0f;
+				}
+				//calculate arclength, add to running total and running total arraylist
+				clength += dpoint.magnitude;
+				arclengths[i] = clength;
+			}
+		}
+
+		totalLength += clength;
+		Debug.Log(totalLength);
+	}
+
 	// Update is called once per frame
 	void Update () {
-		
-		time += DT;
-			
-		// TODO - use time to determine values for u and segment_number in this function call
-		int segmentNumber = Mathf.RoundToInt(time - .5f);
-		if (segmentNumber > (NumberOfPoints - 1)) {
-			segmentNumber--;	
-			time -= NumberOfPoints;
-		}
-		float u = (time - segmentNumber);
-		Debug.Log ($"U: {u}, SegmentNumber: {segmentNumber}, Time: {time}");
-		
-		Vector3 temp = ComputePointOnCatmullRomCurve(u,segmentNumber);
+		int nextPoint = nextTravelPoint++ % travelPoints.Count;
+		Vector3 temp = travelPoints [nextPoint];
 		transform.forward = temp - transform.position;
+		Debug.Log ($"Speed:{(temp - transform.position).magnitude}");
 		transform.position = temp;
+
+//		time += DT;
+//			
+//		// TODO - use time to determine values for u and segment_number in this function call
+//		int segmentNumber = Mathf.RoundToInt(time - .5f);
+//		if (segmentNumber > (NumberOfPoints - 1)) {
+//			segmentNumber--;	
+//			time -= NumberOfPoints;
+//		}
+//		float u = (time - segmentNumber);
+//
+//		
+//		Vector3 temp = ComputePointOnCatmullRomCurve(u,segmentNumber);
+//		transform.forward = temp - transform.position;
+
+//		transform.position = temp;
 	}
 }
