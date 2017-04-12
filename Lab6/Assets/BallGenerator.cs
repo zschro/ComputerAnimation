4d1566c73e6e4f69a3dfc78644c352afae0c9bdf
Zachary Schroeder <z.schro@gmail.com>
2017-04-12 13:48:55 -0400

    setup framework for algorithm

--- Lab6/Assets/BallGenerator.cs
+++ Lab6/Assets/BallGenerator.cs
@@ -2,40 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CraneGame : MonoBehaviour {
+public class BallGenerator : MonoBehaviour {
 
     int numberOfCubes = 5;
-    int cubeMinZ = 1;
-    int cubeMaxZ = 10;
-    int cubeMinX = 1;
-    int cubeMaxX = 10;
+    int cubeMinZ = 2;
+    int cubeMaxZ = 14;
+    int cubeMinX = -3;
+    int cubeMaxX = 12;
+	public GameObject[] Balls;
 
     Vector3[] cubeLocations;
 
     void GenerateCubes()
     {
         cubeLocations = new Vector3[numberOfCubes];
-
         for (int i = 0; i < numberOfCubes; i++)
         {
             cubeLocations[i] = new Vector3(Random.Range(cubeMinX, cubeMaxX), .5f, Random.Range(cubeMinZ, cubeMaxZ));
         }
-
-
         for (int i = 0; i < numberOfCubes; i++)
         {
-            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            //cube.transform.localScale -= new Vector3(3f, 3f, 3f);
+			GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+			var mat = cube.GetComponent<MeshRenderer> ().material;
+			mat.color = Color.red;
             cube.transform.position = cubeLocations[i];
+			Balls [i] = cube;
         }
     }
-
-
+		
     // Use this for initialization
     void Start () {
+		Balls = new GameObject[numberOfCubes];
         GenerateCubes();
-
-
 	}
 	
 	// Update is called once per frame
---
+++ Lab6/Assets/Kinematics.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kinematics : MonoBehaviour {
+	float craneBaseHeight = 1.5f;
+	float arm1Length = 6.0f;
+	float arm2Length = 6.0f;
+	float grabberLength = 0.75f;
+	GameObject[] balls;
+	int currentBall = 0;
+	protected enum State{
+		PickUp,
+		Drop,
+		Rest
+	}
+	State state;
+
+	GameObject arm1;
+	GameObject arm2;
+	GameObject claw;
+	GameObject endEffector;
+
+	// Use this for initialization
+	void Start () {
+		balls = this.GetComponents<BallGenerator>()[0].Balls;
+		this.state = State.PickUp;
+
+		this.arm1 = GameObject.Find ("crane_arm_1");
+		this.arm2 = GameObject.Find ("crane_arm_2");
+		this.claw = GameObject.Find ("claw");
+		this.endEffector = GameObject.Find ("end_effector");
+		LogLinkRotations ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (state == State.PickUp) {
+			MoveEndEffectorTo (balls [currentBall].transform.position);
+		} else if (state == State.Drop) {
+			MoveEndEffectorTo (new Vector3(-8.0f,2.0f,8.0f));//drop off position above walled in area
+		}
+
+
+	}
+
+	void MoveEndEffectorTo(Vector3 goalPosition){
+		
+		this.transform.Rotate (new Vector3 (0f, 1.0f, 0f));
+		LogLinkRotations ();
+
+	}
+
+	void LogLinkRotations(){
+		Debug.Log ($"End Effector Position: {endEffector.transform.position}");
+		Debug.Log ($"Base: {this.transform.localRotation.eulerAngles}");
+		Debug.Log ($"Arm1: {arm1.transform.localRotation.eulerAngles}");
+		Debug.Log ($"Arm2: {arm2.transform.localRotation.eulerAngles}");
+		Debug.Log ($"Claw: {claw.transform.localRotation.eulerAngles}");
+	}
+
+}
